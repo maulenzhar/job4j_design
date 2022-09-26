88b88f1a5151769df86e4d176ae580abacedad0f
mazajaja <you@example.com>
2022-09-27 00:33:43 +0600

    5. Очередь на двух стеках [#160 #344639]

--- src/main/java/ru/job4j/collection/list/linkedlist/ForwardLinked.java
+++ src/main/java/ru/job4j/collection/list/linkedlist/ForwardLinked.java
@@ -38,6 +38,11 @@ public class ForwardLinked<T> implements Iterable<T> {
         head = new Node<>(value, head);
     }
 
+    public boolean revert() {
+        /*TODO impl reverts of linked list.*/
+        return false;
+    }
+
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
