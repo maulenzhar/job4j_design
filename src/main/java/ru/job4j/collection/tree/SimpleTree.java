6353e851d4e1a22989a2265f9d49f5da8ab2532b
mzhar <life_thug@mail.ru>
2022-11-28 01:03:41 +0600

    2. Добавить метод boolean isBinary() [#1712 #364959]

--- src/main/java/ru/job4j/collection/tree/SimpleTree.java
+++ src/main/java/ru/job4j/collection/tree/SimpleTree.java
@@ -3,6 +3,7 @@ package ru.job4j.collection.tree;
 import java.util.LinkedList;
 import java.util.Optional;
 import java.util.Queue;
+import java.util.function.Predicate;
 
 public class SimpleTree<E> implements Tree<E> {
     private final Node<E> root;
@@ -23,13 +24,24 @@ public class SimpleTree<E> implements Tree<E> {
     }
 
     @Override
+    public boolean isBinary() {
+        Predicate<Node<E>> pr = node -> node.children.size() > 2;
+        return findByPredicate(pr).isEmpty();
+    }
+
+    @Override
     public Optional<Node<E>> findBy(E value) {
+        Predicate<Node<E>> pr = node -> node.value.equals(value);
+        return findByPredicate(pr);
+    }
+
+    private Optional<Node<E>> findByPredicate(Predicate<Node<E>> condition) {
         Optional<Node<E>> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
+            if (condition.test(el)) {
                 rsl = Optional.of(el);
                 break;
             }
--- src/test/java/ru/job4j/collection/tree/SimpleTreeTest.java
+++ src/test/java/ru/job4j/collection/tree/SimpleTreeTest.java
@@ -33,4 +33,25 @@ public class SimpleTreeTest {
         tree.add(5, 6);
         assertThat(tree.add(2, 6)).isFalse();
     }
+
+    @Test
+    void whenChildIsNotBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(1, 4);
+        assertThat(tree.isBinary()).isFalse();
+    }
+
+    @Test
+    void whenChildIsBinary() {
+        Tree<Integer> tree = new SimpleTree<>(1);
+        tree.add(1, 2);
+        tree.add(1, 3);
+        tree.add(2, 4);
+        tree.add(2, 5);
+        tree.add(3, 6);
+        tree.add(3, 7);
+        assertThat(tree.isBinary()).isTrue();
+    }
 }
