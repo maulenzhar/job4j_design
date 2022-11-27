02d648d7ecff85d9007abb2d744801ae8a02fa7d
mzhar <life_thug@mail.ru>
2022-11-27 18:50:27 +0600

    1. Создать элементарную структуру дерева [#1711]

--- src/main/java/ru/job4j/collection/set/User.java
+++ src/main/java/ru/job4j/collection/set/User.java
@@ -1,6 +1,9 @@
 package ru.job4j.collection.set;
 
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class User {
     private String name;
@@ -10,9 +13,59 @@ public class User {
     public User() {
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return children == user.children && Objects.equals(name, user.name) && Objects.equals(birthday, user.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, children, birthday);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", children=" + children +
+                ", birthday=" + birthday +
+                '}';
+    }
+
     public User(String name, int children, Calendar birthday) {
         this.name = name;
         this.children = children;
         this.birthday = birthday;
+    }
+
+    public static void main(String[] args) {
+        Map<User, Object> map = new HashMap<>(16);
+        Calendar birthday = Calendar.getInstance();
+
+        User user1 = new User("Alex", 21, birthday);
+        int hashCode1 = user1.hashCode();
+        int hash1 = hashCode1 ^ (hashCode1 >>> 16);
+        int bucket1 = hash1 & 15;
+
+        User user2 = new User("Alex", 21, birthday);
+        int hashCode2 = user2.hashCode();
+        int hash2 = hashCode2 ^ (hashCode2 >>> 16);
+        int bucket2 = hash2 & 15;
+
+        map.put(user1, new Object());
+        map.put(user2, new Object());
+
+        System.out.printf("user1 - хэшкод: %s, хэш: %s, бакет: %s", hashCode1, hash1, bucket1);
+        System.out.println();
+        System.out.printf("user2 - хэшкод: %s, хэш: %s, бакет: %s", hashCode2, hash2, bucket2);
+        System.out.println();
+        System.out.println(35 >>> 2);
+        System.out.println(map);
+
+
+
     }
 }
