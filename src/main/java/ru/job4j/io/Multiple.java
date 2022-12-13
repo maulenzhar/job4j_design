85051944915c5b239af269988322c7491880c718
mzhar <life_thug@mail.ru>
2022-12-13 13:36:25 +0600

    0.1. FileOutputStream. [#252488 #368645]

--- src/main/java/ru/job4j/io/Multiple.java
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -4,12 +4,12 @@ import java.io.FileOutputStream;
 
 public class Multiple {
     public static void main(String[] args) {
-        try {
-            FileOutputStream out = new FileOutputStream("result.txt");
-            for (int i = 0; i < 10; i++) {
-                int res = 2 * i;
-                out.write(String.valueOf(res).getBytes());
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
                 out.write(System.lineSeparator().getBytes());
+                for (int j = 1; j < 10; j++) {
+                    out.write(String.valueOf(j * i + " ").getBytes());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
