bd495ce4e51b287872f4684b727a4e020e5ffd56
mzhar <life_thug@mail.ru>
2022-12-13 10:32:42 +0600

    2. Статистика по коллекции. [#45889 #367968]

--- src/main/java/ru/job4j/collection/question/Analize.java
+++ src/main/java/ru/job4j/collection/question/Analize.java
@@ -7,35 +7,26 @@ public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
         Info info = new Info(0, 0, 0);
-        int changed = 0;
-        int deleted = 0;
-        int added = 0;
 
         Map<Integer, String> userPrev = new TreeMap<>();
-        Map<Integer, String> userCurr = new TreeMap<>();
 
         for (User user : previous) {
             userPrev.put(user.getId(), user.getName());
         }
-        for (User user : current) {
-            userCurr.put(user.getId(), user.getName());
-        }
 
-        int j = Math.max(userPrev.size(), current.size());
-        for (int i = 0; i <= j + 1; i++) {
-            String p = userPrev.get(i);
-            String c = userCurr.get(i);
-            if (p != null && c != null && !p.equals(c)) {
-                info.setChanged(++changed);
-                continue;
-            } else if (p != null && c != null && p.equals(c)) {
-                continue;
-            } else if (p != null && c == null) {
-                info.setDeleted(++deleted);
-                continue;
-            } else if (c != null && p == null) {
-                info.setAdded(++added);
-                continue;
+        for (User userCurr : current) {
+            if (!userPrev.containsKey(userCurr.getId())
+                    && previous.size() == current.size()) {
+                info.setDeleted(+1);
+                info.setAdded(+1);
+            } else if (userPrev.containsKey(userCurr.getId())
+                    && previous.size() == current.size()
+                    && !userPrev.containsValue(userCurr.getName())) {
+                info.setChanged(+1);
+            } else if (previous.size() < current.size()) {
+                info.setAdded(+1);
+            } else if (previous.size() > current.size()) {
+                info.setDeleted(+1);
             }
         }
 
