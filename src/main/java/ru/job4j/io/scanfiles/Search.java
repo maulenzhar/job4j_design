5a5873d132df8021f51c5a234e78a3d2b933e949
mzhar <life_thug@mail.ru>
2023-01-04 21:29:59 +0600

    Валидация параметров запуска. [#246865 #377416]

--- src/main/java/ru/job4j/io/scanfiles/Search.java
+++ src/main/java/ru/job4j/io/scanfiles/Search.java
@@ -11,29 +11,33 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Search {
+    public static Path path;
+    public static String extension;
+
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
             throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
         }
-
-        Path path = Paths.get(args[0]);
-        String extension = args[1];
-        isValid(path, extension);
+        isValid(args);
         /*Files.walkFileTree(start, new PrintFiles());*/
         search(path, p -> p.toFile().getName().endsWith(extension)).forEach(System.out::println);
     }
 
-    private static boolean isValid(Path path, String extension) {
-        File file = path.toFile();
+    private static void isValid(String[] args) {
+        path = Paths.get(args[0]);
+        extension = args[1];
+        File file = Search.path.toFile();
         if (!file.exists()) {
-            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+            throw new IllegalArgumentException(String.format("Not exist %s", args[0]));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s",  args[0]));
         }
         Pattern pattern = Pattern.compile("\\..*");
         Matcher matcher = pattern.matcher(extension);
         if (!matcher.find()) {
             throw new IllegalArgumentException(String.format("No such extension %s", extension));
         }
-        return true;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
