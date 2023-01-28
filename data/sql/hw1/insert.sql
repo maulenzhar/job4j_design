21e629783623f3edd6c4fe8ff0823d1af8f17a1c
mzhar <life_thug@mail.ru>
2023-01-29 01:17:23 +0600

    Схема прав пользователи и роли. [#1093]

---
+++ data/sql/hw1/insert.sql
@@ -0,0 +1,8 @@
+insert into role(name) values ('admin');
+insert into users(name) values ('Jhon');
+insert into rules(name) values ('all');
+insert into state(name) values ('closed');
+insert into category(name) values ('ident');
+insert into item(name) values ('application for identification');
+insert into comments(name) values ('remote identification');
+insert into attachs(name) values ('application');
