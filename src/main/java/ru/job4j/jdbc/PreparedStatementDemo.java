ac913ce6946ce9e74a5b6022b837c620903c8753
mzhar <life_thug@mail.ru>
2023-02-28 01:09:28 +0600

    PreparedStatement

---
+++ src/main/java/ru/job4j/jdbc/PreparedStatementDemo.java
@@ -0,0 +1,73 @@
+package ru.job4j.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class PreparedStatementDemo {
+
+    private Connection connection;
+
+    public PreparedStatementDemo() throws Exception {
+        initConnection();
+    }
+
+    public void initConnection() throws Exception {
+        Class.forName("org.postgresql.Driver");
+        String url = "jdbc:postgresql://localhost:5432/idea_db";
+        String login = "postgres";
+        String password = "password";
+        connection = DriverManager.getConnection(url, login, password);
+    }
+
+    public void insert(City city) {
+        try (PreparedStatement statement =
+                     connection.prepareStatement("insert into cities(name, population) values (?, ?)")) {
+            statement.setString(1, city.getName());
+            statement.setInt(2, city.getPopulation());
+            statement.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
+
+class City {
+
+    private int id;
+
+    private String name;
+
+    private int population;
+
+    public City(int id, String name, int population) {
+        this.id = id;
+        this.name = name;
+        this.population = population;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+}
