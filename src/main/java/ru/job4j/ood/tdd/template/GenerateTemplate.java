eb510046f6dba63acfe0532944401030498b4609
mzhar <life_thug@mail.ru>
2023-05-02 00:10:57 +0600

    Задание Шаблонизатор

--- src/main/java/ru/job4j/ood/tdd/template/GenerateTemplate.java
+++ src/main/java/ru/job4j/ood/tdd/template/GenerateTemplate.java
@@ -2,7 +2,7 @@ package ru.job4j.ood.tdd.template;
 
 import ru.job4j.collection.map.Map;
 
-public class GenerateTemplate implements Generator{
+public class GenerateTemplate implements Generator {
     @Override
     public String produce(String template, Map<String, String> args) {
         return null;
